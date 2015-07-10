e29314c55718af969393ba3397037fdbcb47b405
Leah Padgett <lmp0024@yahoo.com>
2015-07-10 12:07:11 -0400

    Checkpoint 44 - unit tests

---
+++ blocstagram-v2/blocstagram-v2Tests/UserTests.m
@@ -0,0 +1,44 @@
+//
+//  UserTests.m
+//  blocstagram-v2
+//
+//  Created by Leah Padgett on 7/10/15.
+//  Copyright (c) 2015 Leah Padgett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+#import "User.h"
+
+@interface UserTests : XCTestCase
+
+@end
+
+@implementation UserTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+-(void)testThatInitializationWorks
+{
+    NSDictionary *sourceDictionary = @{@"id" : @"8675309",
+                                       @"username" : @"d'oh",
+                                       @"full_name" : @"Homer Simpson",
+                                       @"profile_picture" : @"http://www.example.com/example.jpg"};
+    User *testUser = [[User alloc] initWithDictionary:sourceDictionary];
+    
+    XCTAssertEqualObjects(testUser.idNumber, sourceDictionary[@"id"], @"The ID number should equal");
+    XCTAssertEqualObjects(testUser.userName, sourceDictionary[@"username"], @"The username should be equal");
+    XCTAssertEqualObjects(testUser.fullName, sourceDictionary[@"full_name"], @"The full name should be equal");
+    XCTAssertEqualObjects(testUser.profilePictureURL, [NSURL URLWithString:sourceDictionary[@"profile_picture"]], @"The profile picture should be equal");
+    
+}
+
+@end
